ad461487c910c6dbd2eb4343edbb9cc1bd590686
artursem <artur.semeniuk@gmail.com>
2022-06-26 22:23:00 +0200

    format display forecast

--- src/components/HeadingSecondary.tsx
+++ src/components/HeadingSecondary.tsx
@@ -3,7 +3,7 @@ import { Heading as ChakraHeading, HeadingProps } from '@chakra-ui/react';
 
 const HeadingSecondary: FC<HeadingProps> = (props) => {
 	return (
-		<ChakraHeading {...props} as='h2'>
+		<ChakraHeading {...props} as='h2' fontSize='1.2rem'>
 			{props.children}
 		</ChakraHeading>
 	);
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,11 +2,7 @@ import { FC } from 'react';
 import { List as ChakraList, ListProps } from '@chakra-ui/react';
 
 const List: FC<ListProps> = (props) => {
-	return (
-		<ChakraList {...props} as='h1'>
-			{props.children}
-		</ChakraList>
-	);
+	return <ChakraList {...props}>{props.children}</ChakraList>;
 };
 
 export default List;
