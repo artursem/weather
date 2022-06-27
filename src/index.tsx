c70788c691493a75a2fb30069bbf35f519a2aa5a
artursem <artur.semeniuk@gmail.com>
2022-06-27 20:28:58 +0200

    theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ColorModeScript } from '@chakra-ui/react';
 import { App } from './App';
 
 const container = document.getElementById('root');
@@ -8,7 +8,7 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 root.render(
 	<React.StrictMode>
-		<ColorModeScript />
+		<ColorModeScript initialColorMode='dark' />
 		<App />
 	</React.StrictMode>
 );
