02bbad1d802763a0b522991f2e897577e097f582
artursem <artur.semeniuk@gmail.com>
2022-06-27 18:59:07 +0200

    useCurrentPosition

---
+++ src/hooks/useCurrentPosition.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { Method, Query } from 'src/types/app-types';
+
+const useCurrentPosition = () => {
+	const [location, setLocation] = useState<Query>({
+		method: Method.byLocation,
+		geo: { lat: 0, lon: 0 },
+	});
+	const success = (position: GeolocationPosition) => {
+		const newQuery: Query = {
+			method: Method.byLocation,
+			geo: { lat: position.coords.latitude, lon: position.coords.longitude },
+		};
+
+		setLocation(newQuery);
+	};
+	const failure = (err: GeolocationPositionError) => {};
+	navigator.geolocation.getCurrentPosition(success, failure);
+	return location;
+};
+export default useCurrentPosition;
